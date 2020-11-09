f63df72f29f29b81ac240f5193a88e79dddeeb01
Keigo Suzukawa <k.suzukawa@gmail.com>
2020-11-09 18:53:50 +0000

    Amend the target build

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((clojurescript-mode
   (cider-preferred-build-tool . shadow-cljs)
   (cider-default-cljs-repl . shadow)
-  (cider-shadow-default-options . ":app")))
+  (cider-shadow-default-options . "app")))
