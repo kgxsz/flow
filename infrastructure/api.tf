4a664e19c23c68a44c7c53182afa2f0ac7e36250
Keigo Suzukawa <k.suzukawa@gmail.com>
2021-01-06 08:44:25 +0000

    Clean up and organise infrastructure

--- infrastructure/api.tf
+++ infrastructure/api.tf
@@ -13,11 +13,14 @@ resource "aws_api_gateway_resource" "api" {
 }
 
 resource "aws_api_gateway_method" "api_options" {
-  provider      = aws.eu-west-1
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.api.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
+  provider           = aws.eu-west-1
+  rest_api_id        = aws_api_gateway_rest_api.api.id
+  resource_id        = aws_api_gateway_resource.api.id
+  http_method        = "OPTIONS"
+  authorization      = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
 }
 
 resource "aws_api_gateway_method" "api_post" {
@@ -33,9 +36,13 @@ resource "aws_api_gateway_integration" "api_options" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.api.id
   http_method             = aws_api_gateway_method.api_options.http_method
-  integration_http_method = "OPTIONS"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.api.invoke_arn
+  content_handling        = "CONVERT_TO_TEXT"
+  cache_key_parameters    = [
+    "method.request.path.proxy"
+  ]
 }
 
 resource "aws_api_gateway_integration" "api_post" {
@@ -48,7 +55,7 @@ resource "aws_api_gateway_integration" "api_post" {
   uri                     = aws_lambda_function.api.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "deployment" {
+resource "aws_api_gateway_deployment" "api" {
   provider    = aws.eu-west-1
   depends_on  = [
     aws_api_gateway_integration.api_options,
@@ -72,29 +79,33 @@ resource "aws_api_gateway_domain_name" "api" {
 resource "aws_api_gateway_base_path_mapping" "api" {
   provider    = aws.eu-west-1
   api_id      = aws_api_gateway_rest_api.api.id
-  stage_name  = aws_api_gateway_deployment.deployment.stage_name
+  stage_name  = aws_api_gateway_deployment.api.stage_name
   domain_name = aws_api_gateway_domain_name.api.domain_name
 }
 
-resource "aws_iam_policy" "lambda_policy" {
-  provider    = aws.eu-west-1
-  name        = "flow-policy"
-  path        = "/"
-  description = "Lambda execution policy"
-  policy      = file("policies/lambda_policy.json")
+resource "aws_cloudwatch_log_group" "api" {
+  provider = aws.eu-west-1
+  name     = "/aws/lambda/flow"
 }
 
-resource "aws_iam_role" "lambda_role" {
+resource "aws_iam_role" "api" {
   provider           = aws.eu-west-1
-  name               = "flow-role"
-  assume_role_policy = file("policies/lambda_role.json")
+  name               = "flow"
+  assume_role_policy = file("policies/api/role.json")
 }
 
-resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
+resource "aws_iam_policy" "api_logs" {
+  provider    = aws.eu-west-1
+  name        = "flow"
+  path        = "/"
+  policy      = file("policies/api/logs.json")
+}
+
+resource "aws_iam_policy_attachment" "api" {
   provider   = aws.eu-west-1
   name       = "flow"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = aws_iam_policy.lambda_policy.arn
+  roles      = [aws_iam_role.api.name]
+  policy_arn = aws_iam_policy.api_logs.arn
 }
 
 resource "aws_lambda_function" "api" {
@@ -102,14 +113,14 @@ resource "aws_lambda_function" "api" {
   depends_on       = [
     aws_s3_bucket.api,
     aws_s3_bucket_object.api,
-    aws_iam_role.lambda_role,
-    aws_iam_policy.lambda_policy
+    aws_iam_role.api,
+    aws_iam_policy.api_logs
   ]
   s3_bucket        = aws_s3_bucket.api.bucket
   s3_key           = "flow.zip"
   function_name    = "flow"
   description      = "API for flow.keigo.io"
-  role             = aws_iam_role.lambda_role.arn
+  role             = aws_iam_role.api.arn
   handler          = "flow.core"
   source_code_hash = base64sha256("../api/target/flow.zip")
   runtime          = "java8"
