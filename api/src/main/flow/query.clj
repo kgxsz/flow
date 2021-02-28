e539a2e08530846d98d3594185242fbfcdda89f4
Keigo Suzukawa <k.suzukawa@gmail.com>
2021-02-28 10:30:51 +0000

    Introduce a query for getting the current user

--- api/src/main/flow/query.clj
+++ api/src/main/flow/query.clj
@@ -1,30 +1,52 @@
 (ns flow.query
   (:require [flow.domain.user :as user]
             [flow.domain.authorisation :as authorisation]
-            [taoensso.faraday :as faraday]))
+            [taoensso.faraday :as faraday]
+            [medley.core :as medley]))
 
 
 (defmulti handle first)
 
 
-(defmethod handle :user
+(defmethod handle :current-user
+  [[_ {:keys [current-user-id]}]]
+  ;; TODO - Eventually generalise dealing with queries that require a current user
+  (if-let [{:keys [user/id] :as current-user} (user/fetch current-user-id)]
+    {:current-user
+     {id (user/convey-keys current-user current-user)}}
+    {:current-user
+     {}}))
+
+
+#_(defmethod handle :user
   [[_ {:keys [user-id current-user-id]}]]
   (when-not (uuid? user-id)
     (throw (IllegalArgumentException. "Unsupported query parameters.")))
-  (let [user (user/fetch user-id)
+  (let [{:keys [user/id] :as user} (user/fetch user-id)
         current-user (user/fetch current-user-id)]
     {:user
-     {user-id (user/convey-keys user current-user)}}))
+     {id (user/convey-keys current-user user)}}))
+
+
+(defmethod handle :users
+  [[_ {:keys [current-user-id]}]]
+  (if-let [{:keys [user/id] :as current-user} (user/fetch current-user-id)]
+    {:users
+     (->> (user/fetch-all)
+          (map (partial user/convey-keys current-user))
+          (medley/index-by :user/id))}
+    {:users
+     {}}))
 
 
 (defmethod handle :authorisation
   [[_ {:keys [authorisation-id current-user-id]}]]
   (when-not (uuid? authorisation-id)
     (throw (IllegalArgumentException. "Unsupported query parameters.")))
-  (let [authorisation (authorisation/fetch authorisation-id)
-        current-user (user/fetch current-user-id)]
+  (let [current-user (user/fetch current-user-id)
+        {:keys [authorisation/id] :as authorisation} (authorisation/fetch authorisation-id)]
     {:authorisation
-     {authorisation-id (authorisation/convey-keys authorisation current-user)}}))
+     {id (authorisation/convey-keys current-user authorisation)}}))
 
 
 (defmethod handle :default
