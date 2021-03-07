e56e5dcf9723bd5f2b1c4db5c66650fec8055679
Keigo Suzukawa <k.suzukawa@gmail.com>
2021-03-07 13:05:31 +0000

    Provide documentation to db functions

--- api/src/main/flow/db.clj
+++ api/src/main/flow/db.clj
@@ -9,20 +9,16 @@
 
 
 (defn entity-partition
+  "Generates the partitition key for DynamoDB,
+   made up of the given entity type and its id."
   [entity-type id]
   (str (name entity-type) ":" id))
 
 
-(defn put-entity [entity-type entity-id entity]
-  (faraday/put-item
-   config
-   :flow
-   {:partition (entity-partition entity-type entity-id)
-    :entity (faraday/freeze entity)})
-  entity-id)
-
-
-(defn fetch-entity [entity-type entity-id]
+(defn fetch-entity
+  "Fetches an entity from DynamoDB if it exists.
+   If it doesn't exists, returns nil."
+  [entity-type entity-id]
   (:entity
    (faraday/get-item
     config
@@ -30,7 +26,37 @@
     {:partition (entity-partition entity-type entity-id)})))
 
 
-(defn update-entity [entity-type entity-id f]
+(defn fetch-entities
+  "Fetches all entities of the given entity type."
+  [entity-type]
+  (let [result (faraday/scan
+                config
+                :flow
+                {:attr-conds {:partition [:begins-with (name entity-type)]}})]
+    (mapv :entity result)))
+
+
+(defn put-entity
+  "Puts an entity into DynamoDB if and only if the entity doesn't
+   already exist. On success, returns the entity's id."
+  [entity-type entity-id entity]
+  (if (nil? (fetch-entity entity-type entity-id))
+    (do
+      (faraday/put-item
+       config
+       :flow
+       {:partition (entity-partition entity-type entity-id)
+        :entity (faraday/freeze entity)})
+      entity-id)
+    (throw
+     (IllegalArgumentException.
+      (str "the " entity-type " entity with id " entity-id " already exists.")))))
+
+
+(defn update-entity
+  "Updates an entity by applying function f if and only if the
+   entity exists. On success, returns the entity's id."
+  [entity-type entity-id f]
   (if-let [entity (fetch-entity entity-type entity-id)]
     (do
       (faraday/update-item
@@ -41,13 +67,6 @@
         :expr-attr-vals {":entity" (faraday/freeze (f entity))}
         :return :all-new})
       entity-id)
-    (throw (IllegalArgumentException.
-            (str "the " entity-type " entity with id " entity-id " does not exist.")))))
-
-
-(defn fetch-entities [entity-type]
-  (let [result (faraday/scan
-                config
-                :flow
-                {:attr-conds {:partition [:begins-with (name entity-type)]}})]
-    (mapv :entity result)))
+    (throw
+     (IllegalArgumentException.
+      (str "the " entity-type " entity with id " entity-id " does not exist.")))))
