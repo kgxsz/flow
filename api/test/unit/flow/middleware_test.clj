d5ac93f8f03636b7a758235c69a8286ef6e80a90
Keigo <k.suzukawa@gmail.com>
2021-04-26 19:33:59 +0100

    Add more tests

--- api/test/unit/flow/middleware_test.clj
+++ api/test/unit/flow/middleware_test.clj
@@ -42,3 +42,29 @@
   (testing "The wrapped handler returns the response when the request method is POST."
     (let [handler' (wrap-request-method handler)]
       (is (= response (handler' request))))))
+
+
+(deftest test-wrap-exception
+
+  (testing "The wrapped handler returns a 400 response when an illegal argument exception is thrown."
+    (let [handler' (wrap-exception (fn [r] (throw (IllegalArgumentException. "hello world"))))]
+      (is (= {:body "{\"error\": \"hello world\"}",
+              :headers {"Content-Type" "application/json; charset=utf-8"},
+              :status 400}
+             (handler' request)))))
+
+  (testing "The wrapped handler returns a 500 response when any other exception is thrown."
+    (let [handler' (wrap-exception (fn [r] (throw (IllegalStateException. "hello world"))))]
+      (is (= {:body "{\"error\": \"Internal error detected.\"}",
+              :headers {"Content-Type" "application/json; charset=utf-8"},
+              :status 500}
+             (handler' request))))
+    (let [handler' (wrap-exception (fn [r] (throw (Exception. "hello world"))))]
+      (is (= {:body "{\"error\": \"Internal error detected.\"}",
+              :headers {"Content-Type" "application/json; charset=utf-8"},
+              :status 500}
+             (handler' request)))))
+
+  (testing "The wrapped handler returns the response when no exceptions are thrown."
+    (let [handler' (wrap-exception handler)]
+      (is (= response (handler' request))))))
